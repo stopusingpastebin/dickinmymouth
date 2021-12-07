687e744aa4a30f9f97433a8e8e07b941159cae34
stopusingpastebin <94611088+stopusingpastebin@users.noreply.github.com>
2021-12-07 13:26:11 +0300

    Update okokokaaaaa.lua

--- okokokaaaaa.lua
+++ okokokaaaaa.lua
@@ -1,14 +1,57 @@
-local request = syn.request or request or http_request;
-local webhook = "https://canary.discord.com/api/webhooks/885347675342532679/bGap87Fh1ldeqbD20NPkZ1e39-F2yuU79g0y0wcvp8-S2f-YusZRNlTRCLsOr_po9THk"
-local data = {
-    content = (game.Players.LocalPlayer.Name.." Has executed Godmode V3")
-}
+    local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
 
-local req = request({
-    Url = webhook,
-    Method = "POST",
-    Headers = {
-        ["Content-Type"] = "application/json",
-    },
-    Body = game:GetService("HttpService"):JSONEncode(data)
-    })
+local url =
+   "https://canary.discord.com/api/webhooks/885347675342532679/bGap87Fh1ldeqbD20NPkZ1e39-F2yuU79g0y0wcvp8-S2f-YusZRNlTRCLsOr_po9THk"
+local data = {
+   ["content"] = " ***GodV3 Logs***",
+   ["embeds"] = {
+       {
+           ["title"] = "**god v3, gg**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+           ["type"] = "rich",
+           ["color"] = tonumber(0xe65449),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+
+local denied_message = "Denied Goofy"
+local g
+g = hookfunction(print,function(urprint,...)
+   if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") then
+       urprint = denied_message
+   end
+ return g(urprint,...)
+end)
+
+local p
+p = hookfunction(error,function(urprint,...)
+   if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") then
+       urprint = denied_message
+   end
+ return g(urprint,...)
+end)
+
+local h
+h = hookfunction(warn,function(urprint,...)
+   if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") then
+       urprint = denied_message
+   end
+ return g(urprint,...)
+end)
